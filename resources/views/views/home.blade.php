0b49e057f113dfb910ed7e036f8ce5435a1825c4
fujiwaramatsuri <92214720+fujiwaramatsuri@users.noreply.github.com>
2022-04-09 17:10:32 +0900

    Add files via upload

---
+++ resources/views/views/home.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <!-- <div class="card-header">{{ __('Dashboard') }}</div> -->
+                <div class="card-body-home">
+<!-- 勤務開始 -->
+                <form action="/start" method="post" class="home-btn A">
+                    @csrf
+                    <input type="submit" 
+                        value="勤務開始" id="buttonA" onclick="
+                            getElementById('buttonA').disabled = true;
+                            getElementById('buttonB').disabled = false;
+                            getElementById('buttonC').disabled = true;
+                            getElementById('buttonD').disabled = false;">
+                        </form>
+                    <input type="submit" class="home-btn D"
+                        value="勤務終了"id="buttonD" disabled onclick="
+                            getElementById('buttonA').disabled = false;
+                            getElementById('buttonB').disabled = true;
+                            getElementById('buttonC').disabled = true;
+                            getElementById('buttonD').disabled = true;">
+                    <input type="submit" class="home-btn B"
+                        value="休憩開始"id="buttonB" disabled onclick="
+                            getElementById('buttonB').disabled = true;
+                            getElementById('buttonC').disabled = false;">
+                    <input type="submit" class="home-btn C"
+                        value="休憩終了"id="buttonC" disabled onclick="
+                            getElementById('buttonB').disabled = false;
+                            getElementById('buttonC').disabled = true;">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <!-- {{ __('You are logged in!') }} -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
